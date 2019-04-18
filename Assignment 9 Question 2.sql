50f7f56ffc51d9fdc70688df73b54eab436c119c
unknown <george.kyle87@gmail.com>
2019-04-18 13:39:27 -0500

    Added Assignment 9 Question 2

---
+++ Assignment 9 Question 2.sql
@@ -0,0 +1,12 @@
+-- Assignment 9 Question 2
+CREATE TRIGGER TRIGGERTWO
+ON Assignment
+FOR INSERT, UPDATE
+AS
+declare @charge money
+select @charge = inserted.ASSIGN_CHG_HOUR from inserted
+IF @charge < 42.00 OR @charge > 142.00
+BEGIN
+	ROLLBACK TRANSACTION
+	RAISERROR('Charge per hour must be at least $42.00 and no more than $142.00', 16, 10)
+END
